47e39db6ca063875370edb3ca9a9c5c28839be04
Damon <damon@edstrom.io>
2025-08-02 17:27:31 -0500

    Trying to fix background in FEM preview

--- src/App.svelte
+++ src/App.svelte
@@ -5,7 +5,7 @@
   import Counter from './lib/Counter.svelte'
 </script>
 
-<main>
+<main class="min-h-screen flex items-center justify-center bg-(--bg-yellow)">
   <div class="font-figtree h-100 w-75 p-4 bg-white text-(--card-light-gray) outline-1 outline-(--card-dark-gray) rounded-2xl flex flex-col items-start text-left relative justify-evenly shadow-[7px_7px_0] shadow-gray-950">
     <img class="class rounded-2xl mb-4" src="{illustImg}" alt="illustration">
     <h4 class="mb-1 p-1 font-bold text-xs self-start bg-(--card-yellow) text-(--card-dark-gray) rounded-sm">Learning</h4>
@@ -27,6 +27,7 @@
   --card-white: hsl(0, 0%, 100%);
   --card-light-gray: hsl(0, 0%, 42%);
   --card-dark-gray: hsl(0, 0%, 7%);
+  --bg-yellow: hsl(47, 88%, 63%);
 
 }
 </style>
